d65848ae46469e85dd60e7ca1c2a9c4b3f6c7d86
liunix <liuyifan.com@gmail.com>
2022-02-21 16:17:49 +0800

    format code

--- class1.cpp
+++ class1.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class foo
-{
-public:
-   string str;
-   foo(string inStr)
-   {
-       str = inStr;
-   }
-   void print()
-   {
-      cout<<str<<endl;
-   }
+class foo {
+   public:
+    string str;
+    foo(string inStr) {
+        str = inStr;
+    }
+    void print() {
+        cout << str << endl;
+    }
 };
 
-
-int main()
-{
-   foo Foo("That's a string");
-   Foo.print();
-   return 0;
+int main() {
+    foo Foo("That's a string");
+    Foo.print();
+    return 0;
 }
